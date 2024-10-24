97675b38ffda86c783c2c7a8bb36fb75d38fe3a4
Andy Bennett <andyjpb@register-dynamics.co.uk>
2024-10-24 17:45:51 +0100

    Use the OData snapshot from August for the consumer filter demo.

--- visualise.py
+++ visualise.py
@@ -43,7 +43,7 @@ from i18n import _
 ### Configuration.
 
 db_pool_size = 3
-db_file      = "/var/spool/dam/bitbucket/stats-wales/statswales-data-grabber/snapshots/2024-05-01--2024-05-04/statswales2.hypercube.sqlite"
+db_file      = "/var/spool/dam/bitbucket/stats-wales/statswales-data-grabber/snapshots/2024-08-12-loaded-2024-10-17/statswales2.hypercube.vacuum.sqlite"
 
 
 ################################################################################
